b4cfc90e7f9a8b580eb26e057a39c65ef0bd0e7c
L0ngg <hohoanglong2508@gmail.com>
2025-09-13 09:13:49 +0700

    longkute them swiper

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -8,44 +8,33 @@ export default function Navbar() {
                 <Link to="/" className="navbar-brand">
                     <img src={logo} alt="" style={{ width: "50px", height: "50px", borderRadius: "50%" }} />
                 </Link>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
 
-                <div className="collapse navbar-collapse" id="navbarNav">
+                <form className="d-flex mx-auto" style={{ maxWidth: "600px", width: "100%", position: "relative" }}>
+                    <input
+                        className="form-control me-2"
+                        placeholder="Tìm sản phẩm..."
+                        aria-label="Search"
+                    />
+                    <i className="fa-solid fa-magnifying-glass" style={{ position: "absolute", right: "20px", top: "50%", translate: "0 -50%" }}></i>
 
-                    <form className="d-flex mx-auto" style={{ maxWidth: "600px", width: "100%", position: "relative" }}>
-                        <input
-                            className="form-control me-2"
-                            placeholder="Tìm sản phẩm..."
-                            aria-label="Search"
-                        />
-                        <i className="fa-solid fa-magnifying-glass" style={{ position: "absolute", right: "20px", top: "50%", translate: "0 -50%" }}></i>
-
-                    </form>
-                    <ul className="navbar-nav gap-2">
-                        <li className="nav-item">
-                            <Link to="/login" className="nav-link">
-                                <i className="fa-regular fa-user"></i>
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/" className="nav-link">
-                                <i className="fa-regular fa-heart"></i>
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link to="/cart" className="nav-link">
-                                <i className="fa-solid fa-cart-shopping"></i>
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
+                </form>
+                <ul className="navbar-nav gap-2">
+                    <li>
+                        <Link to="/login" className="nav-link">
+                            <i className="fa-regular fa-user"></i>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/" className="nav-link">
+                            <i className="fa-regular fa-heart"></i>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/cart" className="nav-link">
+                            <i className="fa-solid fa-cart-shopping"></i>
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     );
