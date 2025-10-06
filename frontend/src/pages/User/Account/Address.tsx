d0cfb50619fe991fc4a6eaa9a422522f9c03eba7
L0ngg <hohoanglong2508@gmail.com>
2025-10-06 23:13:46 +0700

    Longkute chinh sua giao dien user/address

--- frontend/src/pages/User/Account/Address.tsx
+++ frontend/src/pages/User/Account/Address.tsx
@@ -1,22 +1,51 @@
 export default function Address() {
     return (
         <div>
-            <h5 className="mb-4">Địa chỉ giao hàng</h5>
-
-            <div className="mb-3">
-                <label className="form-label">Tên người nhận</label>
-                <input type="text" className="form-control" placeholder="Nguyễn Văn B" />
+            <div className="d-flex justify-content-between text-center border-bottom border-2">
+                <h5 className="mt-2 ms-2">Địa chỉ của tôi</h5>
+                <div className="btn btn-primary mb-3">+ Thêm địa chỉ mới</div>
             </div>
-            <div className="mb-3">
-                <label className="form-label">Số điện thoại</label>
-                <input type="text" className="form-control" placeholder="0987654321" />
+            <p className="m-2 fs-2">Địa chỉ</p>
+            <div className="mb-4 ms-2">
+                <div className="d-flex justify-content-between">
+                    <div>
+                        <span className="border-end pe-2 border-2">Longkute</span>
+                        <span className="ms-2 text-muted">(+84)937211264</span>
+                    </div>
+                    <div>
+                        <a href="#" className="text-decoration-none p-2">Cập nhật</a>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-between mt-1">
+                    <div>
+                        <div className="ms-2">xxx An Dương Vương</div>
+                        <div className="ms-2 text-muted">phường Chợ Quán, TP. Hồ Chí Minh</div>
+                    </div>
+                    <div>
+                        <div className="border p-2 fs-6 cursor-pointer" style={{cursor: "pointer"}}>Thiệt lập mặc định</div>
+                    </div>
+                </div>
             </div>
-            <div className="mb-3">
-                <label className="form-label">Địa chỉ chi tiết</label>
-                <textarea className="form-control" rows={3} placeholder="123 Đường ABC, Quận 1, TP.HCM"></textarea>
+            <div className="mb-4 ms-2 pt-3 border-top border-2">
+                <div className="d-flex justify-content-between">
+                    <div>
+                        <span className="border-end pe-2 border-2">Longkute</span>
+                        <span className="ms-2 text-muted">(+84)937211264</span>
+                    </div>
+                    <div>
+                        <a href="#" className="text-decoration-none p-2">Cập nhật</a>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-between mt-1">
+                    <div>
+                        <div className="ms-2">xxx An Dương Vương</div>
+                        <div className="ms-2 text-muted">phường Chợ Quán, TP. Hồ Chí Minh</div>
+                    </div>
+                    <div>
+                        <div className="border p-2 fs-6 cursor-pointer" style={{cursor: "pointer"}}>Thiệt lập mặc định</div>
+                    </div>
+                </div>
             </div>
-
-            <button className="btn btn-success">Lưu địa chỉ</button>
         </div>
     );
 }
